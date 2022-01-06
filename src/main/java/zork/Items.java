6c385cdafe201cc85e6dc1573c593b203b2e4ff2
alex_m_s <alex_m_s@yahoo.com>
2022-01-06 16:44:34 +0100

    added dropItem and addToInventory

--- src/main/java/zork/Items.java
+++ src/main/java/zork/Items.java
@@ -26,4 +26,12 @@ public class Items {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
--- src/main/java/zork/Player.java
+++ src/main/java/zork/Player.java
@@ -1,18 +1,22 @@
 package zork;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
     private int health;
     private ArrayList<Items> inventory;
     private String name;
+    private int weight;
 
     public Player(int health, ArrayList<Items> inventory, String name) {
-        Items punch = new Items("Your own fists", "PUNCH");
+        Items punch = new Items("Your own fists", "PUNCH", 0);
         this.health = health;
         this.inventory = inventory;
         this.name = name;
         this.inventory.add(punch);
+        this.weight = 100;
     }
 
     public int getHealth() {
@@ -37,5 +41,17 @@ public class Player {
 
     public void setName(String name) {
         this.name = name;
+    }
+    public Boolean addToInventory(Items item){
+        if(this.weight - item.getWeight() >= 0){
+            this.inventory.add(item);
+            return false;
+        }
+        return true;
+    }
+
+    public void dropItem(Items item){
+        List<Items> newList = this.inventory.stream().filter(e -> item.getName().equals(e)).collect(Collectors.toList());
+        setInventory((ArrayList<Items>) newList);
     }
 }
