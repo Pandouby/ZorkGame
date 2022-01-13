95b2cdb23f567a620a2b5570e9900066db64aea0
alex_m_s <alex_m_s@yahoo.com>
2022-01-13 10:36:56 +0100

    added fight class

---
+++ src/main/java/zork/Fight.java
@@ -0,0 +1,35 @@
+package zork;
+
+import java.util.Scanner;
+
+public class Fight {
+    private Enemy enemy;
+    private Player player;
+    private String winner;
+
+    public Fight(Enemy enemy, Player player) {
+        this.enemy = enemy;
+        this.player = player;
+    }
+
+    public Enemy getEnemy() {
+        return enemy;
+    }
+
+    public void setEnemy(Enemy enemy) {
+        this.enemy = enemy;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void attackPlayer(){
+        this.player.listItems();
+        System.out.println(">> Selecct you're Item");
+    }
+}
