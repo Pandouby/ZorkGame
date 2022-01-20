5f797fb8f222d7cff980192305160795ada45c19
Pandouby <57749377+Pandouby@users.noreply.github.com>
2022-01-20 09:34:08 +0100

    added door and lock

--- src/main/java/zork/Lock.java
+++ src/main/java/zork/Lock.java
@@ -6,5 +6,33 @@ public class Lock {
     private Riddle riddle;
     private boolean locked;
 
+    public Lock(Riddle riddle) {
+        this.riddle = riddle;
+        locked = true;
+    }
 
+    public Riddle getRiddle() {
+        return riddle;
+    }
+
+    public void setRiddle(Riddle riddle) {
+        this.riddle = riddle;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    public Boolean lockUnlock () {
+        if(locked){
+            locked = false;
+        }else {
+            locked = true;
+        }
+        return locked;
+    }
 }
--- src/main/java/zork/Zork2.java
+++ src/main/java/zork/Zork2.java
@@ -1,14 +1,23 @@
 package zork;
 
+import zork.riddle.QuadraticEquation;
+import zork.riddle.Riddle;
+import zork.rooms.Room;
+
 import java.util.Scanner;
 
 public class Zork2 {
 
 	public static void main(String[] args) {
+		/*
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to zork\n name: ");
 		Game zorkgame = new Game(scanner.nextLine());
-		zorkgame.play();
+		zorkgame.play();*/
+
+		Riddle riddle = new QuadraticEquation();
+
+		riddle.start();
 	}
 
 }
