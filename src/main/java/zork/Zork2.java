8510d15ffeb2bf76011766b678ee761b13de7213
alex_m_s <alex_m_s@yahoo.com>
2022-01-22 18:15:21 +0100

    Added back function, added try catch

--- src/main/java/zork/Zork2.java
+++ src/main/java/zork/Zork2.java
@@ -1,16 +1,12 @@
 package zork;
 
-import zork.riddle.QuadraticEquation;
-import zork.riddle.Riddle;
-import zork.rooms.Room;
-
 import java.util.Scanner;
 
 public class Zork2 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Welcome to zork\n name: ");
+		System.out.println("Welcome to zork\nname: ");
 		Game zorkgame = new Game(scanner.nextLine());
 		zorkgame.play();
 	}
